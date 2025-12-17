96bac8daff0c0e00e178c032a5fc18b06f767ace
Ba Hoa NGUYEN <nbhapl128844@gmail.com>
2025-12-17 20:34:55 +1030

    Add sending message feature.

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,10 +1,57 @@
 "use client";
 
+import { useState } from "react";
 import { motion } from "framer-motion";
+import { ToastContainer, toast } from "react-toastify";
 
 const ContactPage = () => {
+  const [isSending, setIsSending] = useState(false);
+
+  // form submission part
+  const handleFormSubmission = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const form = e.target as HTMLFormElement;
+    const formData = new FormData(form);
+    setIsSending(true);
+
+    try {
+      const res = await fetch("/api/send", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          name: formData.get("name"),
+          email: formData.get("email"),
+          message: formData.get("message"),
+        }),
+      });
+
+      if (res.ok) {
+        toast.info("Successfully send your message!");
+        form.reset();
+      }
+    } catch {
+      return toast.error("Failed to send your message!");
+    } finally {
+      setIsSending(false);
+    }
+  };
+
   return (
     <main className="relative min-h-screen bg-white dark:bg-gray-950 text-gray-900 dark:text-gray-100 overflow-hidden">
+      {/* Toastify part */}
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+
       {/* Animated background blobs */}
       <motion.div
         animate={{ x: [0, 30, 0], y: [0, -30, 0] }}
@@ -46,6 +93,7 @@ const ContactPage = () => {
           animate={{ opacity: 1, y: 0 }}
           transition={{ delay: 0.2, duration: 0.6 }}
           className="bg-white/80 dark:bg-gray-900/80 backdrop-blur-xl rounded-2xl p-8 border border-gray-200 dark:border-gray-800 shadow-lg"
+          onSubmit={handleFormSubmission}
         >
           <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
             <div className="flex flex-col">
@@ -53,6 +101,7 @@ const ContactPage = () => {
                 Name
               </label>
               <input
+                name="name"
                 required
                 type="text"
                 placeholder="Your Name"
@@ -64,6 +113,7 @@ const ContactPage = () => {
                 Email
               </label>
               <input
+                name="email"
                 required
                 type="email"
                 placeholder="you@example.com"
@@ -77,6 +127,7 @@ const ContactPage = () => {
               Message
             </label>
             <textarea
+              name="message"
               placeholder="Your message..."
               rows={6}
               className="rounded-lg border border-gray-300 dark:border-gray-700 px-4 py-3 focus:outline-none focus:ring-2 focus:ring-blue-400 dark:focus:ring-blue-500 resize-none"
@@ -85,9 +136,15 @@ const ContactPage = () => {
 
           <button
             type="submit"
-            className="cursor-pointer active:scale-95 mt-6 w-full md:w-auto px-6 py-3 rounded-lg bg-blue-500 text-white font-semibold hover:bg-blue-600 transition"
+            disabled={isSending}
+            className={`cursor-pointer active:scale-95 mt-6 w-full md:w-auto px-6 py-3 rounded-lg font-semibold transition 
+                      ${
+                        isSending
+                          ? "bg-gray-400"
+                          : "bg-blue-500 hover:bg-blue-600 text-white"
+                      }`}
           >
-            Send Message
+            {isSending ? "Sending your message..." : "Send message"}
           </button>
         </motion.form>
       </section>
